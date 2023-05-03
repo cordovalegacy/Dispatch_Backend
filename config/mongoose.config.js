0326e1dab8142e9eea711c26a6ed64236ceb21ae
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-03 11:26:42 -0700

    Deploy v.2

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log(`Connected to DB`)
-    }
-    catch(err){
-        console.log(err)
-    }
-}
+mongoose.connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log(`Connected to DB`))
+    .catch((err) => console.log("no connection to db:", err))
